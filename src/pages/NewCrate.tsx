4e2f62d8db6a833f6ccc0f7725d15473fa4514e7
Wyatt Jebef <wyattjebef@Wyatts-MacBook-Pro.local>
2025-10-01 22:09:39 -0700

    Removed footer from new crate page

--- src/pages/NewCrate.tsx
+++ src/pages/NewCrate.tsx
@@ -1,5 +1,4 @@
 import Header from "../components/Header";
-import Footer from "../components/Footer";
 import styles from "./NewCrate.module.css";
 import { CrateProvider } from "../contexts/CrateContext";
 import CrateEditor from "./../components/CrateEditor";
@@ -10,7 +9,6 @@ export default function NewCrate() {
             <div className={styles["main-container"]}>
                 <Header />
                 <CrateEditor />
-                <Footer />
             </div>
         </CrateProvider>
     )
